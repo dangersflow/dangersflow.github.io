7e3467c38798982d080adc2e45c911b85dc09d42
dangersflow <pacog879@hotmail.com>
2021-06-07 02:26:06 -0500

    website version v0.4

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,15 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "1daa9651b7bf0c5464d898f9c44ae8dc",
+  "assets/AssetManifest.json": "d79942ca05776ecc36fe3630ac00d0aa",
+"assets/assets/frankresume.pdf": "ed2c61a92ec2d40275c50e8d9d9b006e",
 "assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/graphics/gradient.png": "ce013d6456f676afb85aff7f8afdf7d1",
 "assets/graphics/me-with-laptop.JPG": "6ccbaca746c8a1fe14eb19440a302dbf",
 "assets/graphics/me.png": "e4fbab2f1430cfd9eeb3d7573d16baa2",
 "assets/graphics/wave.png": "191caa22cdcc95923130f3018a4e51ff",
-"assets/NOTICES": "77ddd85c6c377fb0f67b46cf8b2bd304",
+"assets/NOTICES": "df48f241fe24becdf7f5442fd182178c",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "00bb2b684be61e89d1bc7d75dee30b58",
 "assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "4b6a9b7c20913279a3ad3dd9c96e155b",
@@ -18,9 +19,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "abf453c2a4a02ec53e7645a0b49c6eb8",
-"/": "abf453c2a4a02ec53e7645a0b49c6eb8",
-"main.dart.js": "785cf5f3133cce1b5e24d8afdfbd2338",
+"index.html": "c45b7cc6128c708775d4b827b10ca711",
+"/": "c45b7cc6128c708775d4b827b10ca711",
+"main.dart.js": "d7c7a8cad486d16edf9991caa68e7f67",
 "manifest.json": "7e51e14d1f7cef8fbfa9da8a50363635",
 "version.json": "25bec006ec3bd897d7c772417da2d8ca"
 };
