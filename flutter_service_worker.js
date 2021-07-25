b5c48c8055d15d64f6d6a79488b454973ae708ae
dangersflow <pacog879@hotmail.com>
2021-07-24 21:04:50 -0500

    continued development of website

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "00126d768b691f445aac7297f5a42593",
-"/": "00126d768b691f445aac7297f5a42593",
-"main.dart.js": "60d49cf5b378b6985ab145f5c620fdf8",
+"index.html": "a9864a14f4ad65d6be25c972191fa1ea",
+"/": "a9864a14f4ad65d6be25c972191fa1ea",
+"main.dart.js": "a272c705b4e402b7d18fe372ad19a20b",
 "manifest.json": "7e51e14d1f7cef8fbfa9da8a50363635",
 "version.json": "25bec006ec3bd897d7c772417da2d8ca"
 };
